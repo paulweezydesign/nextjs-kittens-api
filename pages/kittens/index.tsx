8b50dd06fdad7e236fdd6e48056e388f57103baa
paulweezydesign <weezy@paulweezydesign.dev>
2023-03-20 02:24:22 +0000

    fixed routes

--- pages/kittens/index.tsx
+++ pages/kittens/index.tsx
@@ -1,7 +1,7 @@
 import useSWR from 'swr'
 import KittensComponent from '../../components/Kittens'
 import type { Kittens } from '../../interfaces/kittens'
-
+import Navbar from '../../components/Navbar'
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function KittensPage() {
@@ -12,10 +12,13 @@ export default function KittensPage() {
   if (!data) return null
 
   return (
+    <>
+    <Navbar />
     <ul>
       {data.map((p) => (
         <KittensComponent key={p.id} kittens={p} />
       ))}
     </ul>
+    </>
   )
 }
